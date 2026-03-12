2714b8750b1af1fd2ca546e48a6f5860e6d338ad
yumaa <166816254+yumahazza@users.noreply.github.com>
2026-03-12 11:13:05 +0700

    Add files segitiga

---
+++ Praktikum1/Latihan/Segitigas/segitiga.cpp
@@ -0,0 +1,30 @@
+#include <gl/glut.h>
+
+void Segitiga(void){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.10, -0.10, 0.00);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.10, -0.10, 0.00);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.00, 0.10, 0.00);
+	glEnd();
+	glFlush();
+	
+}
+
+int main(int argc, char* argv[]){
+	glutInit(&argc, argv);
+	glutInitWindowSize(640, 480);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutCreateWindow("Membuat Segitiga");
+	glutDisplayFunc(Segitiga);
+	glClearColor(1.0f, 0.3f, 0.0f, 1.0f);
+	glutMainLoop();
+	
+	return 0;
+}
+
+
+
