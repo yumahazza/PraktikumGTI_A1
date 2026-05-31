54fdf8027431f1d96e9ccd15509bf44db1c58d18
yumaa <166816254+yumahazza@users.noreply.github.com>
2026-05-31 15:44:55 +0700

    Add files via upload

---
+++ Praktikum4/Tugas/Tugas1/main.cpp
@@ -0,0 +1,91 @@
+#include <stdlib.h> // standard definitions  
+#include <stdio.h> // C I/O (for sprintf)  
+#include <math.h> // standard definitions  
+#include <GL/glut.h> // GLUT
+  
+double rotAngle = 10; // rotation angle (BEWARE: Global)  
+double rotAngle1 = 10; // rotation angle (BEWARE: Global)  
+
+//------------------------------------------------------------------ 
+// init  
+// Sets up some default OpenGL values.  
+//------------------------------------------------------------------ 
+void init()  
+{  
+glClearColor(0, 0, 0, 0); // background color  
+glClearDepth(1.0); // background depth value  
+  glMatrixMode(GL_PROJECTION);  
+  glLoadIdentity();  
+  gluPerspective(60, 1, 1, 1000); // setup a perspective projection  
+  glMatrixMode(GL_MODELVIEW);  
+  glLoadIdentity();  
+  gluLookAt( // set up the camera  
+        0.0, 0.0, 5.0, // eye position  
+        0.0, 0.0, 0.0, // lookat position  
+        0.0, 1.0, 0.0); // up direction  
+}  
+
+//------------------------------------------------------------------ 
+// display callback function  
+// This is called each time application needs to redraw itself.  
+// Most of the opengl work is done through this function.  
+//------------------------------------------------------------------ 
+void display()  
+{  
+  glClear(  
+    GL_COLOR_BUFFER_BIT | // clear the frame buffer (color)  
+    GL_DEPTH_BUFFER_BIT); // clear the depth buffer (depths)  
+  glPushMatrix(); // save the current camera transform  
+    glRotated(rotAngle, 0, 1, 0); // rotate by rotAngle about y-axis   
+    glRotated(rotAngle1, 1, 0, 0); // rotate by rotAngle about y-axis  
+    glEnable(GL_COLOR_MATERIAL); // specify object color  
+    glColor3f(1.0, 0.1, 0.1); // redish  
+    glutSolidTeapot(1); // draw the teapot  
+  glPopMatrix(); // restore the modelview matrix  
+  glFlush(); // force OpenGL to render now  
+  glutSwapBuffers(); // make the image visible  
+}  
+
+//------------------------------------------------------------------  
+// keyboard callback function  
+// This is called whenever a keyboard key is hit.  
+//------------------------------------------------------------------ 
+void keyboard(unsigned char k, int x, int y)  
+{  
+  switch (k)  
+  {  
+   case 'w':  
+     rotAngle += 10; // increase rotation by 10 degrees  
+     break;  
+   case 's':  
+     rotAngle1 += 10; // increase rotation by 10 degrees  
+     break;  
+   case 'a':  
+     rotAngle1 -= 10; // increase rotation by 10 degrees  
+     break;  
+   case 'd':  
+    rotAngle -= 10; // decrease rotation by 10 degrees  
+    break;  
+   case 'x':  
+    exit(0); // exit  
+  }  
+  glutPostRedisplay(); // redraw the image now  
+} 
+ 
+//-----------------------------------------------------------------  
+// main program  
+// Where everything begins.  
+//------------------------------------------------------------------ 
+int main()  
+{  
+  glutInitDisplayMode( // initialize GLUT  
+     GLUT_DOUBLE | // use double buffering  
+     GLUT_DEPTH | // request memory for z-buffer  
+     GLUT_RGB ); // set RGB color mode  
+  glutCreateWindow("GLUT Example"); // create the window  
+  glutDisplayFunc(display); // call display() to redraw window  
+  glutKeyboardFunc(keyboard); // call keyboard() when key is hit  
+  init(); // our own initializations  
+  glutMainLoop(); // let GLUT take care of everything  
+  return 0;  
+} 
